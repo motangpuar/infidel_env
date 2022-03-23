de40f4f988339c0e5854658507aac2c7a0d06cd9
infidel <yma@ieee.org>
2022-03-24 00:01:51 +0700

    using symlink instead of hard copy

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-cp ./.vimrc /home/$(whoami)/
-cp -r ./.vim/ /home/$(whoami)/
-cp -r ./.tmux.conf /home/$(whoami)/
+# cp ./.vimrc /home/$(whoami)/
+# cp -r ./.vim/ /home/$(whoami)/
+# cp -r ./.tmux.conf /home/$(whoami)/
 
+echo "VIM Conf"
+ln -sf $(pwd)/.vimrc /home/$(whoami)/
+ln -sf $(pwd)/.vim/ /home/$(whoami)/
+
+echo "TMUX Conf"
+ln -sf $(pwd)/.vimrc /home/$(whoami)/
+ln -sf $(pwd)/.tmux.conf /home/$(whoami)/
+
+echo "TMUX Plugin"
 mkdir -p "/home/$(whoami)/.tmux/plugins"
 [ -d "/home/$(whoami)/.tmux/plugins/tpm" ] || git clone https://github.com/tmux-plugins/tpm /home/$(whoami)/.tmux/plugins/tpm
